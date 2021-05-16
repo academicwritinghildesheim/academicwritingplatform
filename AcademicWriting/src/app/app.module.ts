4a43675da3db28f7d7eb8a5627cbb80d27c3539e
Brezelkoenig <ritzmann@uni-hildesheim.de>
2021-05-16 19:12:43 +0200

    Modal added

--- AcademicWriting/src/app/app.module.ts
+++ AcademicWriting/src/app/app.module.ts
@@ -13,6 +13,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,9 @@ import {MatTooltipModule} from '@angular/material/tooltip';
     MatButtonModule,
     MatIconModule,
     MatListModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatDialogModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AcademicWriting/src/app/editor/editor.component.ts
+++ AcademicWriting/src/app/editor/editor.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.scss']
 })
-export class EditorComponent implements OnInit {
+export class EditorComponent {
+  constructor(public dialog: MatDialog) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  openDialog() {
+    this.dialog.open(DialogElementsExampleDialog);
+  }
+ 
+}
+@Component({
+  selector: 'dialog-elements-example-dialog',
+  templateUrl: 'dialog-elements-example-dialog.html',
+})
+export class DialogElementsExampleDialog {
+  constructor(public dialog: MatDialog) {}
+  
+ closeDialog(){
+    this.dialog.closeAll()
   }
 
 }
