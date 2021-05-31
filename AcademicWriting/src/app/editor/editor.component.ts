5583d1bc56b93cee57001c7f29ba3ae8d800412c
Brezelkoenig <ritzmann@uni-hildesheim.com>
2021-05-31 17:14:41 +0200

    timer set up funktion, markdown cheat sheet

--- AcademicWriting/src/app/editor/editor.component.ts
+++ AcademicWriting/src/app/editor/editor.component.ts
@@ -1,5 +1,6 @@
-import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import {MatDialog} from '@angular/material/dialog';
 import {Component} from '@angular/core';
+import {DialogComponent} from './components/dialog/dialog.component'
 
 @Component({
   selector: 'app-editor',
@@ -9,35 +10,113 @@ import {Component} from '@angular/core';
 export class EditorComponent {
   constructor(public dialog: MatDialog) {}
 
-  markdown = `## Markdown __rulez__!
+  markdown = `# Markdown Cheat Sheet
 
-### Syntax highlight
-\`\`\`typescript
-const language = 'typescript';
-\`\`\`
-
-### Lists
-1. Ordered list
-2. Another bullet point
-   - Unordered list
-   - Another unordered bullet
-
-### Blockquote
-> Blockquote to the max`;
+  Thanks for visiting [The Markdown Guide](https://www.markdownguide.org)!
+  
+  This Markdown cheat sheet provides a quick overview of all the Markdown syntax elements. It can’t cover every edge case, so if you need more information about any of these elements, refer to the reference guides for [basic syntax](https://www.markdownguide.org/basic-syntax) and [extended syntax](https://www.markdownguide.org/extended-syntax).
+  
+  ## Basic Syntax
+  
+  These are the elements outlined in John Gruber’s original design document. All Markdown applications support these elements.
+  
+  ### Heading
+  
+  # H1
+  ## H2
+  ### H3
+  
+  ### Bold
+  
+  **bold text**
+  
+  ### Italic
+  
+  *italicized text*
+  
+  ### Blockquote
+  
+  > blockquote
+  
+  ### Ordered List
+  
+  1. First item
+  2. Second item
+  3. Third item
+  
+  ### Unordered List
+  
+  - First item
+  - Second item
+  - Third item
+  
+  ### Code
+  
+  \`code\`
+  
+  ### Horizontal Rule
+  
+  ---
+  
+  ### Link
+  
+  [title](https://www.example.com)
+  
+  ### Image
+  
+  ![alt text](image.jpg)
+  
+  ## Extended Syntax
+  
+  These elements extend the basic syntax by adding additional features. Not all Markdown applications support these elements.
+  
+  ### Table
+  
+  | Syntax | Description |
+  | ----------- | ----------- |
+  | Header | Title |
+  | Paragraph | Text |
+  
+  ### Fenced Code Block
+  
+  \`\`\`
+  {
+    "firstName": "John",
+    "lastName": "Smith",
+    "age": 25
+  }
+  \`\`\`
+  
+  ### Footnote
+  
+  Here's a sentence with a footnote. [^1]
+  
+  [^1]: This is the footnote.
+  
+  ### Heading ID
+  
+  ### My Great Heading {#custom-id}
+  
+  ### Definition List
+  
+  term
+  : definition
+  
+  ### Strikethrough
+  
+  ~~The world is flat.~~
+  
+  ### Task List
+  
+  - [x] Write the press release
+  - [ ] Update the website
+  - [ ] Contact the media
+  `;
 
   public openDialog(): void {
-    this.dialog.open(DialogElementsExampleDialogComponent);
+    this.dialog.open(DialogComponent);
   }
 }
 
-@Component({
-  selector: 'app-dialog-elements-example-dialog',
-  templateUrl: 'dialog-elements-example-dialog.html',
-})
-export class DialogElementsExampleDialogComponent {
-  constructor(public dialogRef: MatDialogRef<DialogElementsExampleDialogComponent>) {}
 
-  public closeDialog(): void {
-    this.dialogRef.close();
-  }
-}
+
