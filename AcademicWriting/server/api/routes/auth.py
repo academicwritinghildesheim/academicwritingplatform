e66217bda4058a054392815ace5aad450031a68d
naustica <nick.haupka@outlook.de>
2021-08-07 10:39:21 +0200

    update routes

--- AcademicWriting/server/api/routes/auth.py
+++ AcademicWriting/server/api/routes/auth.py
@@ -1,12 +1,12 @@
-from flask import Blueprint, jsonify, make_response, request, session
+from flask import Blueprint, jsonify, request, session
 from api.database.user import User, user_schema, users_schema
 from api import db, bcrypt
 
 bp = Blueprint('auth', __name__, url_prefix='/api')
 
+
 @bp.route('/login', methods=['POST'])
 def login():
-    json_data = request.json
     user = User.query.filter_by(username=request.values['username']).first()
     if user and bcrypt.check_password_hash(
             user.password, request.values['password'].encode('utf-8')):
@@ -15,6 +15,7 @@ def login():
     else:
         status = False
     return jsonify({'result': status})
+
 
 @bp.route('/logout')
 def logout():
